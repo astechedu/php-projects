44274c240f51c55f5f96bf52654ece50d5be9c5c
astechedu <astech.utube@gmail.com>
2024-09-04 02:53:46 +0530

    Laravel 11 Shopping Cart with vue

--- routes/web.php
+++ routes/web.php
@@ -1,53 +1,27 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
+use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ProductController;
-use App\Http\Controllers\CartController;
+use Inertia\Inertia;
 
-//use App\Mail\MyTestEmail;
-//use Illuminate\Support\Facades\Mail;
-
-//Route::get('/', function () {
-    //return view('welcome');
-//});
-
-Route::get('/', [ProductController::class, 'index']);
-
-Route::get('/products', [ProductController::class, 'index'])->name('products.index');
-Route::get('/products/create', [ProductController::class, 'create'])->name('products.create');
-Route::post('/products', [ProductController::class, 'store'])->name('products.store');
-Route::get('/products/{product}/edit', [ProductController::class, 'edit'])->name('products.edit');
-Route::patch('/products/{product}', [ProductController::class, 'update'])->name('products.update');
-Route::delete('/products/{product}', [ProductController::class, 'destroy'])->name('products.destroy');
-
-//Cart
-Route::get('cart', [CartController::class, 'index'])->name('cart.index');
-Route::post('addtocart', [CartController::class, 'addToCart'])->name('cart.addtocart');
-
-Route::get('cartitems', [CartController::class, 'cartItemCount'])->name('cart.counter');
-Route::delete('remove', [CartController::class, 'cartItemRemove'])->name('cart.remove');
-
-
-//Sending mail to user
-
-Route::get('sendmail', [CartController::class, 'sendmail'])->name('cart.sendmail');
-
-//OR
-//Sending Email Testing
-/*
-Route::get('/testroute', function() {
-    $mailData['name'] = "Funny Coder";
-    $mailData['title'] = "Testing Mail";
-    //The email sending is done using the to method on the Mail facade
-    Mail::to('ajaysisaudiya@gmail.com')->send(new MyTestEmail($mailData));
-    return 'Email sent!';
+Route::get('/', function () {
+    return Inertia::render('Welcome', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
+    ]);
 });
-*/
-/// End of sending mail
 
-//Event and listener testing
-Route::get('usernotify', [CartController::class, 'userNotify']);
-Route::get('adminnotify', [CartController::class, 'AdminNotify']);
-Route::get('eventlistener', [CartController::class, 'eventlistener']);
+Route::get('/dashboard', function () {
+    return Inertia::render('Dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
+require __DIR__.'/auth.php';
