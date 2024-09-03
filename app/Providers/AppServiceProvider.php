61e50a99d34c93b208037cafab7a4f3089dd754a
astechedu <astech.utube@gmail.com>
2024-09-04 02:43:58 +0530

    Laravel 11 Shopping Cart with blade

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Providers;
+
+use App\Events\PostEvent;
+use App\Listeners\PostEventNotification;
+use App\Listeners\UserNotify;
+use App\Listeners\AdminNotify;
+
+use Illuminate\Support\ServiceProvider;
+
+use Illuminate\Support\Facades\Event;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        //
+        Event::listen(            
+            PostEventNotification::class,
+            UserNotify::class,
+            AdminNotify::class,
+        );        
+
+    }
+}
+
+//protected $listen = [
+//'App\Event\UserCreated' => [
+      //'App\Listener\SendEmail',
+    //],
+//];
