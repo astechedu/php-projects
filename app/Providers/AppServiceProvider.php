3e81c588f5e8aae0d4bd3ed74ec96b0d929de356
astechedu <astech.utube@gmail.com>
2024-09-04 02:58:24 +0530

    Laravel 11 Shopping Cart with react

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,14 +2,7 @@
 
 namespace App\Providers;
 
-use App\Events\PostEvent;
-use App\Listeners\PostEventNotification;
-use App\Listeners\UserNotify;
-use App\Listeners\AdminNotify;
-
 use Illuminate\Support\ServiceProvider;
-
-use Illuminate\Support\Facades\Event;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -27,17 +20,5 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
-        Event::listen(            
-            PostEventNotification::class,
-            UserNotify::class,
-            AdminNotify::class,
-        );        
-
     }
 }
-
-//protected $listen = [
-//'App\Event\UserCreated' => [
-      //'App\Listener\SendEmail',
-    //],
-//];
