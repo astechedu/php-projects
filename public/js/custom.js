93eb0844e3b0d276c007f5d6034ca906c1d3fe2a
astechedu <astech.utube@gmail.com>
2024-09-23 01:18:44 +0530

    Laravel 11 Shopping Cart with jQuery

--- public/js/custom.js
+++ public/js/custom.js
@@ -213,19 +213,9 @@ axios({
 //Payment pages
 //Checkout Page
 $('button#checkout').on('click', function(e) {
-	callPHP([1,3,4]);
-	function callPHP(params) {
-		var httpc = new XMLHttpRequest(); // simplified for clarity
-		var url = "payment";
-		httpc.open("POST", url, true); // sending as POST
-	
-		httpc.onreadystatechange = function() { //Call a function when the state changes.
-			if(httpc.readyState == 4 && httpc.status == 200) { // complete and no errors
-				///alert(httpc.responseText); // some processing here, or whatever you want to do with the response
-			}
-		};
-		httpc.send(params);
-	}
+    //e.preventDefault();
+	window.location.href='payment'
+
 });
 
 //Register Form 
